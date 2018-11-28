11698da487c0692a99d330cd7eeec3901a81acf7
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-11-28 15:46:29 -0500

    Updated for latest roles

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Update latest Roles
+rm -rf roles/*
+git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
+cp -R ansible-operator-roles/roles/postgresql-ocp ./roles
+cp -R ansible-operator-roles/roles/gogs-ocp ./roles
+cp ansible-operator-roles/playbooks/gogs.yaml ./playbook.yaml
+rm -rf ansible-operator-roles
+
+# Now build the Operator
 operator-sdk build quay.io/wkulhanek/gogs-operator:v0.0.1
 docker push quay.io/wkulhanek/gogs-operator:v0.0.1
