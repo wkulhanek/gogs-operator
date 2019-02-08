3c812ea6830fa8312761482dabd9efe333d8d6d0
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-02-07 20:38:26 -0500

    Updated for Operator SDK v0.4.0

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ mkdir roles
 git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
 cp -R ansible-operator-roles/roles/postgresql-ocp ./roles
 cp -R ansible-operator-roles/roles/gogs-ocp ./roles
-cp ansible-operator-roles/playbooks/gogs.yaml ./playbook.yaml
+cp ansible-operator-roles/playbooks/gogs.yaml ./playbook.yml
 rm -rf ansible-operator-roles
 
 # Now build the Operator
-operator-sdk build quay.io/wkulhanek/gogs-operator:v0.0.2
-docker push quay.io/wkulhanek/gogs-operator:v0.0.2
+operator-sdk build quay.io/wkulhanek/gogs-operator:v0.0.4
+docker push quay.io/wkulhanek/gogs-operator:v0.0.4
